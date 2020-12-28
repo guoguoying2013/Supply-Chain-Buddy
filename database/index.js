f61439840df45703318177b384ec95a95793e740
Ying Guo <guoguoying2013@gmail.com>
2020-12-27 20:54:59 -0800

    index.js for database, original collections setup

--- database/index.js
+++ database/index.js
@@ -2,6 +2,12 @@ Database = require('arangojs').Database;
 
 const db = new Database();
 db.useDatabase('supply_chain_buddy');
-const purchaseOrderCollection= db.collection('purchase_order_collection');
+const PurchaseOrders = db.collection('purchase_orders'); // po_number(index), status, documents, tracking
+const Notes = db.collection('notes'); // user_id(foreign key), po_number(index), note
+const Users = db.collection('users'); // user_id(index), company_name, username, password(hash with salt), email, customers/suppliers
+const Relationship = db.collection('relationship'); // customer(index), supplier(index)
 
-module.exports = purchaseOrderCollection;
+module.exports = PurchaseOrders;
+module.exports = Notes;
+module.exports = Users;
+module.exports = Relationship;
