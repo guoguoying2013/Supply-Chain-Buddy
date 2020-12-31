78abffd1b882d32c5afc75818044f029f0dd599c
Ying Guo <guoguoying2013@gmail.com>
2020-12-31 00:12:47 -0800

    finalized messages collection

--- database/index.js
+++ database/index.js
@@ -5,10 +5,21 @@ const db = new Database({
 });
 db.useDatabase('supply_chain_buddy');
 db.useBasicAuth("root", null);
-const PurchaseOrders = db.collection('purchase_orders'); // po_number(index), status, documents, tracking
-const Notes = db.collection('notes'); // user_id(foreign key), po_number(index), note
-const accounts = db.collection('accounts'); // user_id(index), company_name, username, password(hash with salt), email, customers/suppliers
-const Relationship = db.collection('relationship'); // customer(index), supplier(index)
+
+const Accounts = db.collection('accounts');
+// user_id(index), company_name, username, password(hash with salt), email, role: customer/supplier
+const Orders = db.collection('orders');
+// order_number(index), 
+// status(issued, received, released_to_purchasing, material_ready, released_to_production, assembled, shipped, closed)
+// documents
+// tracking
+// order_issue_date, required_shipping_date, 
+// customer_id, vendor_id
+// level of urgency(normal, important, urgent, when to send out late notice)
+const Messages = db.collection('messages');
+// order_number(index), writer_name, message, created_date
+const Relationship = db.collection('relationship');
+// customer(index), vendor_id(index)
 
 /*
 accounts
@@ -20,10 +31,26 @@ accounts
   email: 'guoguoying2013@gmail.com',
   role: 'customer',
 }
+
+orders
+{
+  order_number: 1,
+  status: 'released_to_production',
+  documents: null,
+  tracking: null,
+  order_issue_date: 'Sat Dec 26 2020 11:53:50 GMT-0800 (Pacific Standard Time)',
+  required_shipping_date: 'Wed Dec 30 2020 11:53:50 GMT-0800 (Pacific Standard Time)',
+  customer_id: 1,
+  vendor_id: 2,
+  urgency_level: 'important',
+}
+
+messages
+
 */
 
 module.exports.scb = db;
-module.exports.PurchaseOrders = PurchaseOrders;
-module.exports.Notes = Notes;
-module.exports.Accounts = accounts;
+module.exports.Orders = Orders;
+module.exports.Messages = Messages;
+module.exports.Accounts = Accounts;
 module.exports.Relationship = Relationship;
