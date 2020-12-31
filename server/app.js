8d70d17fd6ad80ee52012df91978f909612f5ad9
Ying Guo <guoguoying2013@gmail.com>
2020-12-31 00:17:13 -0800

    added route for messages, to get messages from database when client sents request

--- server/app.js
+++ server/app.js
@@ -11,54 +11,79 @@ app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true,
-    // cookie: {maxAge: 60000}, //that is one min
+    cookie: {maxAge: 60000}, //that is one min
 }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static(path.resolve(__dirname, '../client/dist')));
 
-app.get('/', (req, res) => {
-    res.status(200).send('received');
+app.get('/home', (req, res) => {
+  if(req.session.loggedin) {
+    res.status(200).send(req.session.user_id);
+    } else {
+    res.send('Please login');
+    }
+  res.end();
 })
 
-app.post('/login', async (req, res, next) => {
-    var username = req.body.username;
-    var password = req.body.password;
-    console.log('/login username', username);
-    console.log('/login password', password);
+app.get('/messages', async (req, res) => {
+  if(req.session.loggedin) {
+    let order_number = Number(req.query.order_number);
     try {
-      let account = await db.scb.query(aql`FOR d IN accounts FILTER d.username == ${username} AND d.password == ${password} RETURN d.user_id`);
-      let user_id = await account.all();
-      console.log(user_id);
-      res.send({user_id: user_id, username: username});
+      let order_messages = await db.scb.query(aql`FOR m IN messages FILTER m.order_number == ${order_number} RETURN m`);
+      let messages = await order_messages.all();
+      res.send(messages);
     } catch (err) {
       console.log(err);
       res.status(404).send(err);
     }
-    // if (username && password) {
-    //     db.scb.query({
-    //         query: "FOR d in @c FILTER d.username == @u AND d.password == @p RETURN d",
-    //         bindVars: { c: db.Accounts, u: username, p: password },
-    //     })
-    //       .then((cursor) => {
-    //           console.log('this is cursor: ', cursor);
-    //           res.send(cursor);
-    //         //   return cursor;
-    //           return cursor.forEach((id) => {
-    //             console.log('this is id: ', id);
-    //           })
-    //       })
-    //       .catch((err) => {
-    //         console.log(err);
-    //         res.send(err);
-    //       })
-    // }
+  }
+})
+
+app.get('/orders', async (req, res) => {
+  if(req.session.loggedin) {
+    let user_id = Number(req.query.user_id);
+    console.log('req.query at app.get ', req.query);
+    try {
+      let pos_orders = await db.scb.query(aql`FOR po IN orders FILTER po.customer_id == ${user_id} RETURN po`);
+      let pos = await pos_orders.all();
+      let cos_orders = await db.scb.query(aql`FOR co IN orders FILTER co.vendor_id == ${user_id} RETURN co`);
+      let cos = await cos_orders.all();
+      res.send({
+        purchase_orders: pos,
+        customer_orders: cos,
+      })
+    } catch (err) {
+      console.log(err);
+      res.status(404).send(err);
+    }
+  }
+})
+
+app.post('/login', async (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
+  try {
+    let account = await db.scb.query(aql`FOR d IN accounts FILTER d.username == ${username} AND d.password == ${password} RETURN d`);
+    let user = await account.all();
+    if (user[0].user_id > 0) {
+    req.session.loggedin = true;
+    req.session.username = username;
+    req.session.user_id = [user[0].user_id];
+    res.redirect('/home');
+    } else {
+    res.send('Incorrect Username and/or Password!');
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(404).send(err);
+  }
 })
 
 app.post('/signup', (req, res, next) => {
-    var username = req.body.username;
-    var password = req.body.password;
-    res.send('WIP')
+  var username = req.body.username;
+  var password = req.body.password;
+  res.send('WIP')
 })
 
 module.exports = app;
