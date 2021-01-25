d9343c9b08f55289afc4411e4f4e5fc6130aafdd
Ying Guo <guoguoying2013@gmail.com>
2021-01-25 13:29:58 -0800

    Refactored to use React hooks for better code readability

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,95 +1,65 @@
-import React from 'react';
-// import axios from 'axios';
+import React, { useState } from 'react';
 import LoginForm from './LoginForm.jsx';
 import Dashboard from './Dashboard.jsx';
-import UserProfileBox from './UserProfileBox.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLogin: false,
-      signinFormShow: false,
-      userId: '',
-      username: null,
-      showSuppliersState: false,
-    }
-    this.toggleSigninModal = this.toggleSigninModal.bind(this);
-    this.passUsername = this.passUsername.bind(this);
-    this.logout = this.logout.bind(this);
-  }
+const App = () => {
+  const [isLogin, toggleLogin] = useState(false);
+  const [signinFormShow, toggleSingForm] = useState(false);
+  const [userId, setUserId] = useState('');
+  const [username, setUsername] = useState(null);
+  const [showDashBoard, toggleDashBoard] = useState(false);
 
-  toggleSigninModal(userId) {
-    if(this.state.signinFormShow) {
-      this.setState({
-        signinFormShow: false,
-      });
-      if(userId) {
-        console.log('received userId: ', userId, 'updating state')
-        this.setState({
-          isLogin: true,
-          userId: userId,
-        })
+  const toggleSigninModal = (uId) => {
+    if (signinFormShow) {
+      toggleSingForm(false);
+      if (uId) {
+        toggleLogin(true);
+        setUserId(uId);
+        toggleDashBoard(true);
       }
     } else {
-      this.setState({
-        signinFormShow: true,
-      });
+      toggleSingForm(true);
     }
-  }
+  };
 
-  logout() {
-    this.setState({
-      isLogin: false,
-    })
-  }
-
-  passUsername(u) {
-    this.setState({
-      username: u
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="nav">
-          <i id="logo" class="far fa-lightbulb"></i>
-          <span id="supply-chain">SupplyChain</span>
-          <span id="buddy">Buddy</span>
-          {!this.state.isLogin && (
-            <button onClick={this.toggleSigninModal}>Login</button>
-          )}
-          {this.state.isLogin && (
-            <button onClick={this.logout}>Logout</button>
-          )}
-        </div>
-        <LoginForm 
-          show={this.state.signinFormShow}
-          closeModal={this.toggleSigninModal}
-          passUsername={this.passUsername}
-        />
-        <div className="dashboard">
-          {this.state.isLogin && (
-            <Dashboard
-              userId={this.state.userId}
-              username={this.state.username}
-            />
-          )}
-          {!this.state.isLogin && (
-            <div>
-              <div className="first-page-logo">
-                <i id="logo" class="far fa-lightbulb"></i>
-                <span id="supply-chain">SupplyChain</span>
-                <span id="buddy">Buddy</span>
-              </div>
-              <img id="first-page" src='./backgroundFinal.png'/>
-            </div>
-          )}
-        </div>
+  return (
+    <div>
+      <div className="nav">
+        <i id="logo" className="far fa-lightbulb" />
+        <span id="supply-chain">SupplyChain</span>
+        <span id="buddy">Buddy</span>
+        {!isLogin && (
+          <button onClick={toggleSigninModal} type="button">Login</button>
+        )}
+        {isLogin && (
+          <button onClick={() => { toggleLogin(false); }} type="button">Logout</button>
+        )}
       </div>
-    )
-  }
-}
+      <LoginForm
+        show={signinFormShow}
+        closeModal={toggleSigninModal}
+        passUsername={(u) => { setUsername(u); }}
+      />
+      <div className="dashboard">
+        {showDashBoard && (
+          <Dashboard
+            userId={userId}
+            username={username}
+          />
+        )}
+      </div>
+      {!isLogin && (
+        <div>
+          <div className="first-page-logo">
+            <i id="logo" className="far fa-lightbulb" />
+            <span id="supply-chain">SupplyChain</span>
+            <span id="buddy">Buddy</span>
+          </div>
+          <img id="first-page" src="./backgroundFinal.png" alt="Homepage background" />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
