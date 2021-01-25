7d4ee5d4216f05ff612bf6a916d3e9f5c99a4ca0
Ying Guo <guoguoying2013@gmail.com>
2021-01-25 10:24:17 -0800

    refactored UserProfileBox to React hook

--- client/src/components/UserProfileBox.jsx
+++ client/src/components/UserProfileBox.jsx
@@ -1,49 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-class UserProfileBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userInfo: null,
-    };
-    this.fetchUser = this.fetchUser.bind(this);
-  }
+const UserProfileBox = ({ userId }) => {
+  console.log('UserProfileBox is called');
+  const [userInfo, setUserInfo] = useState(null);
 
-  componentDidMount() {
-    this.fetchUser();
-  }
-
-  fetchUser() {
-    axios.get('/user', {
+  useEffect(() => {
+    axios.get('/api/user', {
       params: {
-        user_id: this.props.userId
-      }
+        user_id: userId,
+      },
     })
       .then((response) => {
-        this.setState({
-          userInfo: response.data[0]
-        })
-        console.log('this.state.userInfo: ', this.state.userInfo);
+        console.log('response: ', response);
+        setUserInfo(response.data[0]);
       })
       .catch((err) => {
         console.log(err);
-      })
-  }
+      });
+  }, []);
 
-  render() {
-    return(
-      <div className='user-profile-box'>
-        {this.state.userInfo && (
-          <div>
-            <img className="your-img" src={this.state.userInfo.photo_url}></img>
-            <h3 className="your-company">{this.state.userInfo.company_name}</h3>
-            <span className="greeting">Hello, {this.state.userInfo.username}!</span>
-          </div>
-        )}
+  return (
+    <div className="user-profile-box">
+      {userInfo && (
+      <div>
+        <img className="your-img" src={userInfo.photo_url} alt="user" />
+        <h3 className="your-company">{userInfo.company_name}</h3>
+        <span className="greeting">
+          Hello,
+          {userInfo.username}
+          !
+        </span>
       </div>
-    )
-  }
-}
+      )}
+    </div>
+  );
+};
 
 export default UserProfileBox;
