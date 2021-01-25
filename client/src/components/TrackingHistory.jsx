3097551483136a0bd8a4c5b63f48a07884f7dea4
Ying Guo <guoguoying2013@gmail.com>
2021-01-25 10:39:19 -0800

    refactored to functional component from class component

--- client/src/components/TrackingHistory.jsx
+++ client/src/components/TrackingHistory.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
 
-class TrackingHistory extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    if(this.props.tracking === 'Not Available') {
-      return (
-        <div className="tracking-history">
-          Tracking info is not available for this order.
-        </div>
-      )
-    }
-    return(
+const TrackingHistory = (props) => {
+  if (props.tracking === 'Not Available') {
+    return (
       <div className="tracking-history">
-        {this.props.trackingHistory.reverse().map((t) => {
-          return(
-            <div className="tracking-history-single-point">
-              <i className="far fa-clock">&nbsp;</i>
-              <span> {t.checkpoint_time.slice(0, 10)} &nbsp;&nbsp;</span>
-              <i className="fas fa-truck">&nbsp;</i>
-              <span> {t.location}</span>
-              <br />
-              <span className="tracking-history-status"> {t.message}</span>
-              <br />
-            </div>
-          )
-        })}
+        Tracking info is not available for this order.
       </div>
-    )
+    );
   }
-}
+  return (
+    <div className="tracking-history">
+      {props.trackingHistory.reverse().map((t) => (
+        <div className="tracking-history-single-point">
+          <i className="far fa-clock">&nbsp;</i>
+          <span>
+            {t.checkpoint_time.slice(0, 10)}
+            &nbsp;
+            &nbsp;
+          </span>
+          <i className="fas fa-truck">&nbsp;</i>
+          <span>
+            {t.location}
+          </span>
+          <br />
+          <span className="tracking-history-status">
+            {t.message}
+          </span>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default TrackingHistory;
