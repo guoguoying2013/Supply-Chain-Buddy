ab628d637b1816ea894a3dbd1a4563329c9dc795
Ying Guo <guoguoying2013@gmail.com>
2021-01-25 16:05:49 -0800

    cleaned code

--- client/src/components/EnterNewOrderModal.jsx
+++ client/src/components/EnterNewOrderModal.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable radix */
+/* eslint-disable camelcase */
 import React from 'react';
 import axios from 'axios';
 
 class EnterNewOrderModal extends React.Component {
   constructor(props) {
     super(props);
-    this.state= {};
+    this.state = {};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -13,91 +15,94 @@ class EnterNewOrderModal extends React.Component {
     e.preventDefault();
     this.setState({
       [e.target.name]: e.target.value,
-    })
+    });
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    let newOrderEntry = {
+    const newOrderEntry = {
       documents: null,
       tracking: null,
       order_issue_date: Date(),
       urgency_level: 'normal',
       status: 'issued',
-    }
+    };
 
     Object.assign(newOrderEntry, this.state);
-    let customer_id_int = parseInt(newOrderEntry.customer_id);
+    const customer_id_int = parseInt(newOrderEntry.customer_id);
     newOrderEntry.customer_id = customer_id_int;
-    let vendor_id_int = parseInt(newOrderEntry.vendor_id);
+    const vendor_id_int = parseInt(newOrderEntry.vendor_id);
     newOrderEntry.vendor_id = vendor_id_int;
-    let order_number_int = parseInt(newOrderEntry.order_number);
+    const order_number_int = parseInt(newOrderEntry.order_number);
     newOrderEntry.order_number = order_number_int;
+    const total_int = parseInt(newOrderEntry.total);
+    newOrderEntry.total = total_int;
 
-    for (let i = 0; i < this.props.customers_obj.length; i++) {
-        if(this.props.customers_obj[i].customer_id === newOrderEntry.customer_id) {
-            console.log('customer i:', this.props.customers_obj[i])
-            newOrderEntry.customer_company = this.props.customers_obj[i].customer_company;
-            newOrderEntry.customer_photo_url = this.props.customers_obj[i].customer_photo_url;
-        }
+    for (let i = 0; i < this.props.customers_obj.length; i += 1) {
+      if (this.props.customers_obj[i].customer_id === newOrderEntry.customer_id) {
+        newOrderEntry.customer_company = this.props.customers_obj[i].customer_company;
+        newOrderEntry.customer_photo_url = this.props.customers_obj[i].customer_photo_url;
+      }
     }
     for (let i = 0; i < this.props.suppliers_obj.length; i++) {
-        if(this.props.suppliers_obj[i].vendor_id === newOrderEntry.vendor_id) {
-            console.log('supplier i: ', this.props.suppliers_obj[i])
-            newOrderEntry.vendor_name = this.props.suppliers_obj[i].vendor_company;
-            newOrderEntry.vendor_photo_url = this.props.suppliers_obj[i].vendor_photo_url;
-        }
+      if (this.props.suppliers_obj[i].vendor_id === newOrderEntry.vendor_id) {
+        newOrderEntry.vendor_name = this.props.suppliers_obj[i].vendor_company;
+        newOrderEntry.vendor_photo_url = this.props.suppliers_obj[i].vendor_photo_url;
+      }
     }
     console.log('after looping', newOrderEntry);
-    axios.post('/orders', newOrderEntry)
+    axios.post('/api/orders', newOrderEntry)
       .then((res) => {
         console.log('res at post new order modal: ', res);
         this.props.toggleForm(e);
       })
       .catch((err) => {
         console.log('axios err: ', err);
-      })
+      });
   }
 
   render() {
     return (
       <div className="LoginModal">
         <form className="new-order-modal-content">
-        <h1>Enter New Order</h1>
+          <h1>Enter New Order</h1>
           <label>Purchase Order Number</label>
           <br />
-          <input type="text" value={this.state.order_number} onChange={this.handleChange} name="order_number"></input>
+          <input type="text" value={this.state.order_number} onChange={this.handleChange} name="order_number" />
           <br />
           <label>Customer</label>
           <br />
           <select value={this.state.customer_id} onChange={this.handleChange} name="customer_id">
-            <option></option>
-            {this.props.customers.map((c) => {
-              return(<option value={c[1]} name="customer_id">{c[0]}</option>)
-            })}
+            {this.props.customers.map((c) => (<option value={c[1]} name="customer_id">{c[0]}</option>))}
           </select>
           <br />
           <label>Supplier</label>
           <br />
           <select value={this.state.supplier_id} onChange={this.handleChange} name="vendor_id">
-            <option></option>
-            {this.props.suppliers.map((s) => {
-              return(<option value={s[1]} name="vendor_id">{s[0]}</option>)
-            })}
+            {this.props.suppliers.map((s) => (<option value={s[1]} name="vendor_id">{s[0]}</option>))}
           </select>
+          <br />
+          <label>Description</label>
+          <br />
+          <input type="text" value={this.state.description} onChange={this.handleChange} name="description" />
           <br />
           <label>Total Amount</label>
           <br />
-          <input type="number" value={this.state.total} onChange={this.handleChange} name='total'></input>
+          <input type="number" value={this.state.total} onChange={this.handleChange} name="total" />
           <br />
           <label>Expected Shipping Date</label>
           <br />
-          <input type="date" value={this.state.required_shipping_date} onChange={this.handleChange} name='required_shipping_date'></input>
+          <input type="date" value={this.state.required_shipping_date} onChange={this.handleChange} name="required_shipping_date" />
           <br />
-          <button onClick={this.handleSubmit} className="submit-button">Submit</button>
+          <div id="button-left">
+            <button onClick={this.handleSubmit} className="submit-button" type="submit">Submit</button>
+          </div>
+          <div id="button-right">
+            <button onClick={this.props.toggleForm} className="submit-button" type="button">Cancel</button>
+          </div>
         </form>
       </div>
-    )
+    );
   }
 }
 
